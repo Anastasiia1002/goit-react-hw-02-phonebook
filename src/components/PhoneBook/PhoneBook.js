2c653fb0b5a1a34af8e1d3c3a2805fe5c85a5fa5
Anastasiia <106449970+Anastasiia1002@users.noreply.github.com>
2022-09-16 18:15:33 +0300

    add

---
+++ src/components/PhoneBook/PhoneBook.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import { nanoid } from 'nanoid';
+import PropTypes from 'prop-types';
+import ContactForm from './ContactForm/ContactForm';
+import Filter from './Filter/Filter';
+import ContactList from './ContactList/ContactList';
+
+class PhoneBook extends React.Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  formSubmitHandler = ({ name, number }) => {
+    const lowerCaseName = name.toLowerCase();
+    let someContacts = this.state.contacts;
+    if (someContacts.some(e => e.name.toLowerCase() === lowerCaseName)) {
+      return alert(`${name} is already in contacts`);
+    } else {
+      const contact = {
+        id: nanoid(),
+        name,
+        number,
+      };
+      this.setState(prevState => ({
+        contacts: [...prevState.contacts, contact],
+      }));
+      console.log(contact);
+    }
+  };
+
+  handleFilterChange = event => {
+    const { name, value } = event.currentTarget;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  filterContacts = () => {
+    let filterContact = [];
+    if (this.state.filter) {
+      filterContact = this.state.contacts.filter(
+        contact =>
+          contact.name.includes(this.state.filter) ||
+          contact.name.toLowerCase().includes(this.state.filter)
+      );
+    } else {
+      return this.state.contacts;
+    }
+    return filterContact;
+  };
+
+  delContact = id => {
+    this.setState(prev => ({
+      contacts: prev.contacts.filter(el => el.id !== id),
+    }));
+  };
+  render() {
+    const { filter } = this.state;
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmitForm={this.formSubmitHandler} />
+
+        <h2>Contacts</h2>
+        <Filter filter={filter} handleFilterChange={this.handleFilterChange} />
+        <ContactList
+          filterContacts={this.filterContacts}
+          delContact={this.delContact}
+        />
+      </div>
+    );
+  }
+}
+PhoneBook.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  filter: PropTypes.string.isRequired,
+  formSubmitHandler: PropTypes.func.isRequired,
+  delContact: PropTypes.func.isRequired,
+  filterContacts: PropTypes.func.isRequired,
+  handleFilterChange: PropTypes.func.isRequired,
+};
+export default PhoneBook;
